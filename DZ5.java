724f0abb42e89f6e316614369da8182408f0b95b
RomanOresh <romanoreshchenko@gmail.com>
2024-07-05 10:16:44 +0200

    Initial commit

---
+++ DZ5.java
@@ -0,0 +1,12 @@
+public class DZ5 {
+    public static void main(String[] args) {
+
+        int a = 0;
+
+        for (int i = 1; i<=6; i++){
+            a += i;
+            System.out.println(i + ") Num is " + i + ", sum is " + a);
+        }
+        System.out.println("---------------------- \n" + "Sum of numbers is " + a);
+    }
+}
